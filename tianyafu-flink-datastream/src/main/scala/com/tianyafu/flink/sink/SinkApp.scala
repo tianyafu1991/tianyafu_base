0dfcfbd82f18ece444076bc37b05dbd60628086f
tianyafu1991 <1193835093@qq.com>
2021-04-04 11:20:03 +0800

    feat:增加20201104 Flink 04的代码 Flink Sink

--- tianyafu-flink-datastream/src/main/scala/com/tianyafu/flink/sink/SinkApp.scala
+++ tianyafu-flink-datastream/src/main/scala/com/tianyafu/flink/sink/SinkApp.scala
@@ -1,15 +1,21 @@
 package com.tianyafu.flink.sink
 
 import com.tianyafu.flink.bean.Domain.Access
+import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.api.common.serialization.SimpleStringEncoder
 import org.apache.flink.streaming.api.scala.{DataStream, KeyedStream, StreamExecutionEnvironment}
 import org.apache.flink.api.scala._
 import org.apache.flink.core.fs.{FileSystem, Path}
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink
 import org.apache.flink.streaming.connectors.redis.RedisSink
 import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
 import org.apache.flink.streaming.connectors.redis.common.mapper.{RedisCommand, RedisCommandDescription, RedisMapper}
+import org.apache.http.HttpHost
+import org.elasticsearch.client.Requests
 
+import java.util
 
 
 object SinkApp {
@@ -35,14 +41,14 @@ object SinkApp {
         Access(splits(0).trim.toLong, splits(1).trim, splits(2).trim.toDouble)
       }).keyBy(_.domain).sum("traffic")
 
-//    stream.print()
+    //    stream.print()
 
     /**
      * 输出到文件
      */
 
-//    stream.writeAsText("out/text",FileSystem.WriteMode.OVERWRITE).setParallelism(1)
-//    stream.writeAsCsv("out/csv",FileSystem.WriteMode.OVERWRITE).setParallelism(1)
+    //    stream.writeAsText("out/text",FileSystem.WriteMode.OVERWRITE).setParallelism(1)
+    //    stream.writeAsCsv("out/csv",FileSystem.WriteMode.OVERWRITE).setParallelism(1)
 
     /*stream.addSink(StreamingFileSink.forRowFormat(
     new Path("out/text2"),
@@ -50,17 +56,50 @@ object SinkApp {
     ).build())*/
 
 
+    /**
+     * Flink 输出到Redis
+     */
+
+    /*val conf = new FlinkJedisPoolConfig.Builder().setHost("hadoop01").setPort(16379).setDatabase(4).build()
+    stream.addSink(new RedisSink[Access](conf, new RedisExampleMapper))*/
 
 
-    val conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(16379).setDatabase(4).build()
-    stream.addSink(new RedisSink[Access](conf, new RedisExampleMapper))
+    /**
+     * 写入到MySQL中
+     */
+    //    stream.addSink(new TianyafuMySQLSink)
+
+
+    //开启容错
+    env.enableCheckpointing(5000)
+
+    val httpHosts = new util.ArrayList[HttpHost]()
+    httpHosts.add(new HttpHost("hadoop01", 9200))
+
+    val esSinkFunction = new ElasticsearchSinkFunction[Access] {
+      override def process(t: Access, runtimeContext: RuntimeContext, requestIndexer: RequestIndexer): Unit = {
+        val json = new util.HashMap[String, String]()
+        json.put("domain", t.domain)
+        json.put("traffics", t.traffic + "")
+
+        val request = Requests.indexRequest()
+          .index("tianyafu_flink_access")
+          .source(json)
+          .id(t.domain) // 流处理这块要注意 要自定义id  因为流处理这里是带状态的 我们只要最后的结果
+
+        requestIndexer.add(request)
+
+      }
+    }
+
+    stream.addSink(new ElasticsearchSink.Builder[Access](httpHosts, esSinkFunction).build())
 
 
     env.execute(getClass.getSimpleName)
   }
 
 
-  class RedisExampleMapper extends RedisMapper[Access]{
+  class RedisExampleMapper extends RedisMapper[Access] {
     override def getCommandDescription: RedisCommandDescription = {
       new RedisCommandDescription(RedisCommand.HSET, "tianyafu_flink_traffics")
     }
--- tianyafu-flink-datastream/src/main/scala/com/tianyafu/flink/sink/TianyafuMySQLSink.scala
+++ tianyafu-flink-datastream/src/main/scala/com/tianyafu/flink/sink/TianyafuMySQLSink.scala
@@ -1,7 +1,13 @@
 package com.tianyafu.flink.sink
 
 import com.tianyafu.flink.bean.Domain.Access
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
+import com.tianyafu.flink.utils.MySQLUtils
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+import scalikejdbc.{DB, SQL}
+import scalikejdbc.config.DBs
+
+import java.sql.Connection
 
 /**
  * @Author:tianyafu
@@ -9,5 +15,30 @@ import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
  * @Description:
  */
 class TianyafuMySQLSink extends RichSinkFunction[Access]{
+
+
+  override def open(parameters: Configuration): Unit = {
+    DBs.setupAll()
+  }
+
+  override def close(): Unit = {
+    DBs.closeAll()
+  }
+
+  /**
+   * 这里用了replace  into  要求domain字段是唯一性约束
+   * @param value
+   * @param context
+   */
+  override def invoke(value: Access, context: SinkFunction.Context[_]): Unit = {
+    DBs.setupAll()
+    DB.localTx{
+      implicit session => {
+        SQL("replace into tianyafu_traffic(domain,traffics) values(?,?)")
+          .bind(value.domain,value.traffic)
+          .update().apply()
+      }
+    }
+  }
 
 }
