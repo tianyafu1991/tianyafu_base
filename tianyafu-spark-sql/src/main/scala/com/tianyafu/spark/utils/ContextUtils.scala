043b32ac4a70196190f4a622ad78381e72ce5bdc
tianyafu1991 <1193835093@qq.com>
2021-11-14 10:43:01 +0800

    feat:增加spark-sql和spark-streaming的相关module

---
+++ tianyafu-spark-sql/src/main/scala/com/tianyafu/spark/utils/ContextUtils.scala
@@ -0,0 +1,5 @@
+package com.tianyafu.spark.utils
+
+object ContextUtils {
+
+}
---
+++ tianyafu-spark-streaming/src/main/scala/com/tianyafu/spark/ss/basic/StreamingSocketWCApp.scala
@@ -0,0 +1,20 @@
+package com.tianyafu.spark.ss.basic
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object StreamingSocketWCApp {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName(getClass.getSimpleName).setMaster("local[2]")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("hadoop", 9527)
+    lines.flatMap(_.split(",")).map((_,1)).reduceByKey(_+_).print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
